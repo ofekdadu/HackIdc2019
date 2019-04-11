8e91e7a189544ca6851052f7a101f65c6d12e3d8
michsebag <michsebag@gmail.com>
2019-04-11 23:57:43 +0300

    Fofek with coat compared to all images of Fofek

--- casadepapel.py
+++ casadepapel.py
@@ -20,12 +20,17 @@ import timeit
 
 img1 = cv2.imread('fofek1.png')[:,:,::-1]
 img2 = cv2.imread('fofek2.png')[:,:,::-1]
+img3 = cv2.imread('fofek3.png')[:,:,::-1]
+
 
 img1_location = api.human_locations(img1)
 img2_location = api.human_locations(img2)
+img3_location = api.human_locations(img3)
+
 
 img_1_human = api.crop_human(img1, img1_location)
 img_2_human = api.crop_human(img2, img2_location)
+img_3_human = api.crop_human(img3, img3_location)
 
 
 # In[3]:
@@ -43,6 +48,10 @@ plt.imshow(human_1_1)
 human_1_2 = img_2_human[0]
 plt.imshow(human_1_2)
 
+# human 1 photo 2
+human_1_3 = img_3_human[0]
+plt.imshow(human_1_3)
+
 
 # In[5]:
 
@@ -57,6 +66,8 @@ plt.imshow(human_1_2)
 t1 = timeit.default_timer()
 human_1_1_vector = api.human_vector(human_1_1)
 human_1_2_vector = api.human_vector(human_1_2)
+human_1_3_vector = api.human_vector(human_1_3)
+
 t2 = timeit.default_timer()
 print('Time elapsed: {} sec'.format(round(t2 - t1, 3)))
 
@@ -66,6 +77,14 @@ print('Time elapsed: {} sec'.format(round(t2 - t1, 3)))
 # In[7]:
 
 
+print("Fofek with sweatshirt vs Fofek with T-shirt:")
 print(api.human_distance(human_1_1_vector, human_1_2_vector))
+
+print("Fofek with sweatshirt vs Fofek with coat:")
+print(api.human_distance(human_1_1_vector, human_1_3_vector))
+
+print("Fofek with T-shirt vs Fofek with coat:")
+print(api.human_distance(human_1_3_vector, human_1_2_vector))
+
 
 
