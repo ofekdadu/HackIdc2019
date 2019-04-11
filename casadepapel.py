2f1792d453bed38622de4a00fdad73380b17c468
tamirc <tamirc3@gmail.com>
2019-04-12 02:30:59 +0300

    commmit message2

--- casadepapel.py
+++ casadepapel.py
@@ -18,97 +18,97 @@ import timeit
 # In[2]:
 
 
-# img1 = cv2.imread('fofek1.png')[:,:,::-1]
-# img2 = cv2.imread('fofek2.png')[:,:,::-1]
-# img3 = cv2.imread('fofek3.png')[:,:,::-1]
-# img4 = cv2.imread('fofek4.png')[:,:,::-1]
+# img1 = cv2.imread('fofek1.jpeg')[:,:,::-1]
+# img2 = cv2.imread('fofek2.jpeg')[:,:,::-1]
+# img3 = cv2.imread('fofek3.jpeg')[:,:,::-1]
+# img4 = cv2.imread('fofek4.jpeg')[:,:,::-1]
 #
-# img5 = cv2.imread('fofeklookalike.png')[:,:,::-1]
+# img5 = cv2.imread('fofeklookalike.jpeg')[:,:,::-1]
 
 
 # ---------1
-ofek1 = cv2.imread('ofek1.png')[:,:,::-1]
-ofek2 = cv2.imread('ofek2.png')[:,:,::-1]
-ofek3 = cv2.imread('ofek3.png')[:,:,::-1]
+ofek1 = cv2.imread('ofek1.jpeg')[:,:,::-1]
+ofek2 = cv2.imread('ofek2.jpeg')[:,:,::-1]
+ofek3 = cv2.imread('ofek3.jpeg')[:,:,::-1]
 
-ofek4 = cv2.imread('ofek4.png')[:,:,::-1]
-ofek5 = cv2.imread('ofek5.png')[:,:,::-1]
-ofek6 = cv2.imread('ofek6.png')[:,:,::-1]
-
-#  ------------
-alon1 = cv2.imread('alon1.png')[:,:,::-1]
-alon2 = cv2.imread('alon2.png')[:,:,::-1]
-alon3 = cv2.imread('alon3.png')[:,:,::-1]
-
-alon4 = cv2.imread('alon4.png')[:,:,::-1]
-alon5 = cv2.imread('alon5.png')[:,:,::-1]
-alon6 = cv2.imread('alon6.png')[:,:,::-1]
+ofek4 = cv2.imread('ofek4.jpeg')[:,:,::-1]
+ofek5 = cv2.imread('ofek5.jpeg')[:,:,::-1]
+ofek6 = cv2.imread('ofek6.jpeg')[:,:,::-1]
 
 #  ------------
-tamir1 = cv2.imread('tamir1.png')[:,:,::-1]
-tamir2 = cv2.imread('tamir2.png')[:,:,::-1]
-tamir3 = cv2.imread('tamir3.png')[:,:,::-1]
+alon1 = cv2.imread('alon1.jpeg')[:,:,::-1]
+alon2 = cv2.imread('alon2.jpeg')[:,:,::-1]
+alon3 = cv2.imread('alon3.jpeg')[:,:,::-1]
 
-tamir4 = cv2.imread('tamir4.png')[:,:,::-1]
-tamir5 = cv2.imread('tamir5.png')[:,:,::-1]
-tamir6 = cv2.imread('tamir6.png')[:,:,::-1]
-
-#  ------------
-michael1 = cv2.imread('michael1.png')[:,:,::-1]
-michael2 = cv2.imread('michael2.png')[:,:,::-1]
-michael3 = cv2.imread('michael3.png')[:,:,::-1]
-
-michael4 = cv2.imread('michael4.png')[:,:,::-1]
-michael5 = cv2.imread('michael5.png')[:,:,::-1]
-michael6 = cv2.imread('michael6.png')[:,:,::-1]
+alon4 = cv2.imread('alon4.jpeg')[:,:,::-1]
+alon5 = cv2.imread('alon5.jpeg')[:,:,::-1]
+alon6 = cv2.imread('alon6.jpeg')[:,:,::-1]
 
 #  ------------
-lili1 = cv2.imread('lili1.png')[:,:,::-1]
-lili2 = cv2.imread('lili2.png')[:,:,::-1]
-lili3 = cv2.imread('lili3.png')[:,:,::-1]
+tamir1 = cv2.imread('tamir1.jpeg')[:,:,::-1]
+tamir2 = cv2.imread('tamir2.jpeg')[:,:,::-1]
+tamir3 = cv2.imread('tamir3.jpeg')[:,:,::-1]
 
-lili4 = cv2.imread('lili4.png')[:,:,::-1]
-lili5 = cv2.imread('lili5.png')[:,:,::-1]
-lili6 = cv2.imread('lili6.png')[:,:,::-1]
-
-#  ------------
-alice1 = cv2.imread('alice1.png')[:,:,::-1]
-alice2 = cv2.imread('alice2.png')[:,:,::-1]
-alice3 = cv2.imread('alice3.png')[:,:,::-1]
-
-alice4 = cv2.imread('alice4.png')[:,:,::-1]
-alice5 = cv2.imread('alice5.png')[:,:,::-1]
-alice6 = cv2.imread('alice6.png')[:,:,::-1]
-
+tamir4 = cv2.imread('tamir4.jpeg')[:,:,::-1]
+tamir5 = cv2.imread('tamir5.jpeg')[:,:,::-1]
+tamir6 = cv2.imread('tamir6.jpeg')[:,:,::-1]
 
 #  ------------
-ido1 = cv2.imread('ido1.png')[:, :, ::-1]
-ido2 = cv2.imread('ido2.png')[:, :, ::-1]
-ido3 = cv2.imread('ido3.png')[:, :, ::-1]
+michael1 = cv2.imread('michael1.jpeg')[:,:,::-1]
+michael2 = cv2.imread('michael2.jpeg')[:,:,::-1]
+michael3 = cv2.imread('michael3.jpeg')[:,:,::-1]
 
-ido4 = cv2.imread('ido4.png')[:, :, ::-1]
-ido5 = cv2.imread('ido5.png')[:, :, ::-1]
-ido6 = cv2.imread('ido6.png')[:, :, ::-1]
+michael4 = cv2.imread('michael4.jpeg')[:,:,::-1]
+michael5 = cv2.imread('michael5.jpeg')[:,:,::-1]
+michael6 = cv2.imread('michael6.jpeg')[:,:,::-1]
+
+#  ------------
+lili1 = cv2.imread('lili1.jpeg')[:,:,::-1]
+lili2 = cv2.imread('lili2.jpeg')[:,:,::-1]
+lili3 = cv2.imread('lili3.jpeg')[:,:,::-1]
+
+lili4 = cv2.imread('lili4.jpeg')[:,:,::-1]
+lili5 = cv2.imread('lili5.jpeg')[:,:,::-1]
+lili6 = cv2.imread('lili6.jpeg')[:,:,::-1]
+
+#  ------------
+alice1 = cv2.imread('alice1.jpeg')[:,:,::-1]
+alice2 = cv2.imread('alice2.jpeg')[:,:,::-1]
+alice3 = cv2.imread('alice3.jpeg')[:,:,::-1]
+
+alice4 = cv2.imread('alice4.jpeg')[:,:,::-1]
+alice5 = cv2.imread('alice5.jpeg')[:,:,::-1]
+alice6 = cv2.imread('alice6.jpeg')[:,:,::-1]
+
+
+#  ------------
+ido1 = cv2.imread('ido1.jpeg')[:, :, ::-1]
+ido2 = cv2.imread('ido2.jpeg')[:, :, ::-1]
+ido3 = cv2.imread('ido3.jpeg')[:, :, ::-1]
+
+ido4 = cv2.imread('ido4.jpeg')[:, :, ::-1]
+ido5 = cv2.imread('ido5.jpeg')[:, :, ::-1]
+ido6 = cv2.imread('ido6.jpeg')[:, :, ::-1]
 
 #-----------
 
-bob1 = cv2.imread('bob1.png')[:, :, ::-1]
-bob2 = cv2.imread('bob2.png')[:, :, ::-1]
-bob3 = cv2.imread('bob3.png')[:, :, ::-1]
+bob1 = cv2.imread('bob1.jpeg')[:, :, ::-1]
+bob2 = cv2.imread('bob2.jpeg')[:, :, ::-1]
+bob3 = cv2.imread('bob3.jpeg')[:, :, ::-1]
 
-bob4 = cv2.imread('bob4.png')[:, :, ::-1]
-bob5 = cv2.imread('bob5.png')[:, :, ::-1]
-bob6 = cv2.imread('bob6.png')[:, :, ::-1]
+bob4 = cv2.imread('bob4.jpeg')[:, :, ::-1]
+bob5 = cv2.imread('bob5.jpeg')[:, :, ::-1]
+bob6 = cv2.imread('bob6.jpeg')[:, :, ::-1]
 
 #-----------
 
-alex1 = cv2.imread('alex1.png')[:, :, ::-1]
-alex2 = cv2.imread('alex2.png')[:, :, ::-1]
-alex3 = cv2.imread('alex3.png')[:, :, ::-1]
+alex1 = cv2.imread('alex1.jpeg')[:, :, ::-1]
+alex2 = cv2.imread('alex2.jpeg')[:, :, ::-1]
+alex3 = cv2.imread('alex3.jpeg')[:, :, ::-1]
 
-alex4 = cv2.imread('alex4.png')[:, :, ::-1]
-alex5 = cv2.imread('alex5.png')[:, :, ::-1]
-alex6 = cv2.imread('alex6.png')[:, :, ::-1]
+alex4 = cv2.imread('alex4.jpeg')[:, :, ::-1]
+alex5 = cv2.imread('alex5.jpeg')[:, :, ::-1]
+alex6 = cv2.imread('alex6.jpeg')[:, :, ::-1]
 
 
 def enrichEmbed(img1,img2,img3):
